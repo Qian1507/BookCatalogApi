652cf78f9dddc4dbd4c399ddea51d8d39b019f2c
QIAN LI <liqiancky@outlook.com>
2026-05-06 18:03:54 +0200

    Add project files.

---
+++ BookCatalogApi/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookCatalogApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCatalogApi.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookCatalogApi/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookCatalogApi.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Author { get; set; } = string.Empty;
+
+        [StringLength(50)]
+        public string? Genre { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
