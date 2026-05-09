816b0ae3b7f024a4b05e9fbd3425c4614a50e465
QIAN LI <liqiancky@outlook.com>
2026-05-09 20:43:41 +0200

    DefaultConnection

--- BookCatalogApi/Program.cs
+++ BookCatalogApi/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddOpenApi();
 
